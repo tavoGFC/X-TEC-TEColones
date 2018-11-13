d1c253c21fa481c4d3b249c67e1b76f07a982fc2
Ricardo Chang <fankashai@hotmail.com>
2018-11-12 22:23:09 -0600

    Se logra captar los input en el controlador ECA

--- X-TEC.TEColones/X-TEC.TEColones/Controllers/StorageCenterManager/SCMHomeController.cs
+++ X-TEC.TEColones/X-TEC.TEColones/Controllers/StorageCenterManager/SCMHomeController.cs
@@ -4,14 +4,34 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using X_TEC.TEColones.Models;
+
 namespace X_TEC.TEColones.Controllers.StorageCenterManager
 {
     public class SCMHomeController : Controller
     {
+
+        SCMModel empleado = new SCMModel();
+
         // GET: SCMHome
         public ActionResult Home()
         {
-            return View("~/Views/StorageCenterManager/SCMHome/SCMHome.cshtml");
+            
+            empleado.Name = "Rebeca Beaker";
+            return View("~/Views/StorageCenterManager/SCMHome/SCMHome.cshtml",empleado);
+        }
+
+        [HttpPost]
+        public ActionResult Insertar(int InputCarnet, int InputPyC = 0, int InputPlastico = 0, int InputTetrapack = 0, int InputVidrio = 0) //LOS NOMBRES DE LOS PARAMETROS TIENEN QUE SER LOS NOMBRES DE LOS INPUT EN EL HTML
+        {
+
+            System.Diagnostics.Debug.WriteLine("CARNET INGRESADO: " + InputCarnet);
+            System.Diagnostics.Debug.WriteLine("PYC INGRESADO: " + InputPyC);
+            System.Diagnostics.Debug.WriteLine("PLASTICO INGRESADO: " + InputPlastico);
+            System.Diagnostics.Debug.WriteLine("TETRAPACK INGRESADO: " + InputTetrapack);
+            System.Diagnostics.Debug.WriteLine("VIDRIO INGRESADO: " + InputVidrio);
+
+            return Home();
         }
     }
 }
