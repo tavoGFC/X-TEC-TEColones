b263d414f052690905f622b15926342c5227683b
randyma <randyma01@gmail.com>
2018-11-16 16:27:22 -0600

    Version 1.4 - Configuration - Twitter Connection

---
+++ X-TEC.TEColones/X-TEC.TEColones.Models/AdminModels/Configuration.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace X_TEC.TEColones.Models.AdminModels
+{
+    class Configuration
+    {
+
+        public int ValorPlastico { get; set; }
+
+        public int ValorVidrio { get; set; }
+
+        public int ValorPapel { get; set; }
+
+        public int ValorAluminio { get; set; }
+
+        public int TasaComedor { get; set; }
+
+        public int TasaMatricula { get; set; }
+
+
+
+
+    }
+}
--- X-TEC.TEColones/X-TEC.TEColones.Persistence/TwitterConnection.cs
+++ X-TEC.TEColones/X-TEC.TEColones.Persistence/TwitterConnection.cs
@@ -9,24 +9,26 @@ namespace X_TEC.TEColones.Persistence
 {
     class TwitterConnection
     {
-        public string CONSUMER_KEY { get; set; }
+        public string CONSUMER_KEY;
+        public string CONSUMER_SECRET;
+        public string ACCESS_TOKEN;
+        public string ACCESS_TOKEN_SECRET;
 
-        public string CONSUMER_SECRET { get; set; }
+        public void SetCredentials(string consumerKey, string consumerSecret, string accessToken, string accessTokenSecret)
+        {
+            CONSUMER_KEY = consumerKey;
+            CONSUMER_SECRET = consumerSecret;
+            ACCESS_TOKEN = accessToken;
+            ACCESS_TOKEN_SECRET = accessTokenSecret;
+        }
 
-        public string ACCESS_TOKEN { get; set; }
- 
-        public string ACCESS_TOKEN_SECRET { get; set; }
-
-        public string Message { get; set; }
-
-        //CONSUMER_KEY = "8gYBbQjhYO9JyRbbR3blQnTEd";
-        //CONSUMER_SECRET = "oTFi5zD5YzBk9awJNaKIWwJ4VF3QSSo06mKTCmjmKKa0s0V7KM";
-        //ACCESS_TOKEN = "1062419932820987904-KCxTUQbcQfPFJRLxun56YgnFAKO7H6";
-        //ACCESS_TOKEN_SECRET =  "VkHF7piK0ntXXprmlZixySgN1a8STpoUMfEDHA9iTsuD8";
-
-        //Auth.SetUserCredentials("8gYBbQjhYO9JyRbbR3blQnTEd", "oTFi5zD5YzBk9awJNaKIWwJ4VF3QSSo06mKTCmjmKKa0s0V7KM", "1062419932820987904-KCxTUQbcQfPFJRLxun56YgnFAKO7H6", "VkHF7piK0ntXXprmlZixySgN1a8STpoUMfEDHA9iTsuD8");
-        //var user = User.GetAuthenticatedUser();
-        //var tweet = Tweet.PublishTweet("HOLA CHAAAAAANG.");
+        public void Publish(string mensaje)
+        {
+            //Auth.SetUserCredentials(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET);
+            Auth.SetUserCredentials("8gYBbQjhYO9JyRbbR3blQnTEd", "oTFi5zD5YzBk9awJNaKIWwJ4VF3QSSo06mKTCmjmKKa0s0V7KM", "1062419932820987904-KCxTUQbcQfPFJRLxun56YgnFAKO7H6", "VkHF7piK0ntXXprmlZixySgN1a8STpoUMfEDHA9iTsuD8");
+            var user = User.GetAuthenticatedUser();
+            var tweet = Tweet.PublishTweet(mensaje);
+        }
     }
 }
 
