7f37b79dc82658c7599fbaf0a64cfce55b82e9f9
tavoGFC <gusfcarrera@hotmail.es>
2018-11-22 01:52:16 -0600

    Version 3.3.2 - Crear nuevo usuario estudiante, se verifica la aceptacion de terminos y condiciones.

--- X-TEC.TEColones/X-TEC.TEColones/Controllers/Student/CreateAccountController.cs
+++ X-TEC.TEColones/X-TEC.TEColones/Controllers/Student/CreateAccountController.cs
@@ -15,54 +15,58 @@ namespace X_TEC.TEColones.Controllers.Student
     public class CreateAccountController : Controller
     {
         // GET: CreateAccount
-        public ActionResult CreateAccount(string message, CreateUser student)
+        public ActionResult CreateAccount()
         {
-            ViewBag.Message = message;
+            CreateUser student = new CreateUser();
             return View("~/Views/Student/CreateAccount/CreateAccount.cshtml", student);
         }
+        
+
 
         [HttpPost]
         public ActionResult SignUp(CreateUser student)
-        {        
-            if (student.PhotoFile != null)
-            {
-                Stream stream = student.PhotoFile.InputStream;
-                BinaryReader br = new BinaryReader(stream);
-                byte[] imgByte = br.ReadBytes((Int32)stream.Length);
-                student.ImageByte = imgByte;
-            }
+        {           
+            //if (student.PhotoFile != null)
+            //{
+            //    Stream stream = student.PhotoFile.InputStream;
+            //    BinaryReader br = new BinaryReader(stream);
+            //    byte[] imgByte = br.ReadBytes((Int32)stream.Length);
+            //    student.ImageByte = imgByte;
+            //}
             
 
-            if (DBConnection.InsertStudent(student))
-            {
-                StudentModel studentModel = new StudentModel
-                {
-                    Id = student.Id,
-                    FirstName = student.FirstName,
-                    LastName = student.LastName,
-                    University = student.University,
-                    Headquarter = student.Headquarter,
-                    PhotoBytes = student.ImageByte,
-                    Email = student.Email,
-                    Skills = student.Skills,
-                    Description = student.Description,
-                    PhoneNumber = student.PhoneNumber,
-                    TCS = 0
-                };
-                if (studentModel.PhotoBytes.Count() == 0)
-                {
-                    studentModel.Photo = "http://cdn.onlinewebfonts.com/svg/img_569204.png";
-                }
-                else
-                {
-                    studentModel.RenderImage();
-                }
-                DBConnection.GetBenefit(studentModel);
-                DBConnection.GetMaterial(studentModel);
-                return View("~/Views/Student/Home/Home.cshtml", studentModel);
-            }
+            //if (DBConnection.InsertStudent(student))
+            //{
+            //    StudentModel studentModel = new StudentModel
+            //    {
+            //        Id = student.Id,
+            //        FirstName = student.FirstName,
+            //        LastName = student.LastName,
+            //        University = student.University,
+            //        Headquarter = student.Headquarter,
+            //        PhotoBytes = student.ImageByte,
+            //        Email = student.Email,
+            //        Skills = student.Skills,
+            //        Description = student.Description,
+            //        PhoneNumber = student.PhoneNumber,
+            //        TCS = 0
+            //    };
+            //    if (studentModel.PhotoBytes.Count() == 0)
+            //    {
+            //        studentModel.Photo = "http://cdn.onlinewebfonts.com/svg/img_569204.png";
+            //    }
+            //    else
+            //    {
+            //        studentModel.RenderImage();
+            //    }
+            //    DBConnection.GetBenefit(studentModel);
+            //    DBConnection.GetMaterial(studentModel);
+            //    
+            //    return View("~/Views/Student/Home/Home.cshtml", studentModel);
+            //}
 
-            return CreateAccount("Existe un usuario con el mismo carnet, verifique por favor", student); //PartialView("~/Views/Student/CreateAccount/CreateAccount.cshtml", student);
+            ViewBag.Message = "Existe un usuario con el mismo carnet, verifique por favor";
+            return View("~/Views/Student/CreateAccount/CreateAccount.cshtml", student); //PartialView("~/Views/Student/CreateAccount/CreateAccount.cshtml", student);
         }
         
     }
